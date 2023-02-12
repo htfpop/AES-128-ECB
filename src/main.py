983433fff1f6125364dd903a04f8aa66f146304d
Christopher Leung <chrisleung360@yahoo.com>
2023-02-11 19:46:59 -0800

    initial commit

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import tools
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("-------------------------------")
+    print("CSCI-531 AES-128 Implementation")
+    print("-------------------------------")
+    arr_key = tools.read_file("input\\key.txt")
+    tools.debug_print_arr_ascii(arr_key)
+
+    arr_pt = tools.read_file("input\\plaintext.txt")
+    tools.debug_print_arr_ascii(arr_pt)
+
+"""
+    arr = [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xf, 0x10, 0x11]
+    arr2 = [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xf, 0x10, 0x11]
+    tools.debug_print_arr(arr)
+    result = tools.xor(arr, arr2)
+    tools.debug_print_arr(result)
+"""
+
+
---
+++ src/tools.py
@@ -0,0 +1,61 @@
+# Constants
+AES_KEY_SIZE = 16
+PT_BLOCK_SIZE = 16
+AES_PT_PADDING = 0x00
+
+# Error Codes
+INVALID_KEY_SIZE = 0xDEADDEAD
+
+
+def debug_print_arr_hex(hex_array):
+    for x in range(len(hex_array)):
+        if x % 8 == 0 and x != 0:
+            print("\r\n")
+        print(f'0x{hex_array[x]:02x}', end=' ')
+    print()
+
+
+def debug_print_arr_ascii(hex_array):
+    for x in range(len(hex_array)):
+        if x % 8 == 0 and x != 0:
+            print("\r\n")
+        print(f'{hex_array[x]:c}', end=' ')
+    print()
+
+
+def xor(arr1, arr2):
+    return [a ^ b for a, b in zip(arr1, arr2)]
+
+
+def add(arr1, arr2):
+    return [a + b for a, b in zip(arr1, arr2)]
+
+
+def read_file(path):
+    with open(path, "rb") as f_input:
+        return f_input.read()
+
+
+def read_AES_key(path):
+    with open(path, "rb") as AES_Key:
+        key = AES_Key.read()
+        if len(key) != AES_KEY_SIZE:
+            print(f'[ERROR]: Input AES key is {len(key)} bytes. Should be {AES_KEY_SIZE} bytes. Exiting now...')
+            exit(INVALID_KEY_SIZE)
+        else:
+            return key
+
+
+def read_plaintext(path):
+    with open(path, "rb") as pt:
+        pt = pt.read()
+        if len(pt) % PT_BLOCK_SIZE != 0:
+            print(f'[NOTICE]: Plaintext size is {len(pt)}, padding to nearest multiple of {PT_BLOCK_SIZE}')
+            for i in range(1, len(pt) % PT_BLOCK_SIZE):
+                pt += AES_PT_PADDING
+        return pt
+
+
+def bin_test(hexnum):
+    hexnum += 0x1
+    print(f'Hex + 1 0x{hexnum:02x}')
