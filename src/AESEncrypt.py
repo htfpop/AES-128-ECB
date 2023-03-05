2ad2187539c68e447f7fb4521707415be9b27f45
Christopher Leung <chrisleung360@yahoo.com>
2023-03-05 13:17:56 -0800

    functionality for 2 FIPS enc/dec blocks

--- src/AESEncrypt.py
+++ src/AESEncrypt.py
@@ -458,10 +458,10 @@ def shift_rows_inv(state):
         state[i][3] = int(converter[3])
 
 def test_enc():
-    #key = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c]
-    key = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f] #Test vector FIPS 197
-    #state = [[0x32, 0x88, 0x31, 0xe0], [0x43, 0x5a, 0x31, 0x37], [0xf6, 0x30, 0x98, 0x07], [0xa8, 0x8d, 0xa2, 0x34]]
-    state = [0x00, 0x44, 0x88, 0xcc], [0x11, 0x55, 0x99, 0xdd], [0x22, 0x66, 0xaa, 0xee], [0x33, 0x77, 0xbb, 0xff] #Test vector FIPS197
+    key = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c]
+    #key = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f] #Test vector FIPS 197
+    state = [[0x32, 0x88, 0x31, 0xe0], [0x43, 0x5a, 0x31, 0x37], [0xf6, 0x30, 0x98, 0x07], [0xa8, 0x8d, 0xa2, 0x34]]
+    #state = [0x00, 0x44, 0x88, 0xcc], [0x11, 0x55, 0x99, 0xdd], [0x22, 0x66, 0xaa, 0xee], [0x33, 0x77, 0xbb, 0xff] #Test vector FIPS197
 
     aes_keys = key_expansion(key)
 
@@ -514,7 +514,7 @@ def test_enc():
 
     state = tools.xor_2d(state, round_key)
 
-    for inv_curr_round in range(9,1,-1):
+    for inv_curr_round in range(9,-1,-1):
 
         print(f'[DECRYPT] round{10 - inv_curr_round}: istart')
         tools.debug_print_arr_2dhex_1line(state)
@@ -541,15 +541,43 @@ def test_enc():
         tools.debug_print_arr_2dhex_1line(state)
         print()
 
-        if inv_curr_round != 1:
+        if inv_curr_round != 0:
             print(f'[DECRYPT] round{10 - inv_curr_round}: i_mix_cols')
             state = inv_mix_cols(state)
             tools.debug_print_arr_2dhex_1line(state)
             print()
 
     print(f'AES Decrypt Complete')
-    tools.debug_print_arr_2dhex(state)
+    tools.debug_print_arr_2dhex_1line(state)
     print()
+"""
+    print(f'[DECRYPT] round{10}: istart')
+    tools.debug_print_arr_2dhex_1line(state)
+    print()
+
+    print(f'[DECRYPT] round{10}: is_row')
+    shift_rows_inv(state)
+    tools.debug_print_arr_2dhex_1line(state)
+    print()
+
+    print(f'[DECRYPT] round{10}: is_box')
+    s_box_inv_sub(state)
+    tools.debug_print_arr_2dhex_1line(state)
+    print()
+
+    round_key = extract_key(aes_keys[0])
+
+    print(f'[DECRYPT] round{10}: ik_sch')
+    tools.debug_print_arr_2dhex_1line(round_key)
+    print()
+
+    print(f'[DECRYPT] round{10}: ik_add')
+    state = tools.xor_2d(state, round_key)
+    tools.debug_print_arr_2dhex_1line(state)
+    print()
+"""
+
+
 """
     print(f'[Round 10]: Round key Value')
     round_key = extract_key(aes_keys[0])
