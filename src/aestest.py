e594c1f2d63ba0b0cae1b0f5302fae315a16d0d6
Christopher Leung <chrisleung360@yahoo.com>
2023-03-05 13:25:42 -0800

    moved aes test to aestest.py

--- src/aestest.py
+++ src/aestest.py
@@ -1,22 +1,109 @@
 import tools
+import AESEncrypt
 
-# Press the green button in the gutter to run the script.
+
+
+def test_aes():
+    #key = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c]
+    key = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f] #Test vector FIPS 197
+    #state = [[0x32, 0x88, 0x31, 0xe0], [0x43, 0x5a, 0x31, 0x37], [0xf6, 0x30, 0x98, 0x07], [0xa8, 0x8d, 0xa2, 0x34]]
+    state = [0x00, 0x44, 0x88, 0xcc], [0x11, 0x55, 0x99, 0xdd], [0x22, 0x66, 0xaa, 0xee], [0x33, 0x77, 0xbb, 0xff] #Test vector FIPS197
+
+    aes_keys = AESEncrypt.key_expansion(key)
+
+    round_key = AESEncrypt.extract_key(aes_keys[0])
+    state = tools.xor_2d(state, round_key)
+
+    for curr_round in range(1, 11, 1):
+
+        print(f'[ENCRYPT]: round{curr_round}: Start of Round')
+        tools.debug_print_arr_2dhex_1line(state)
+        print()
+
+        print(f'[ENCRYPT]: round{curr_round}: After SubBytes')
+        AESEncrypt.s_box_sub(state)
+        tools.debug_print_arr_2dhex_1line(state)
+        print()
+
+        print(f'[ENCRYPT]: round{curr_round}: After ShiftRows')
+        AESEncrypt.shift_rows(state)
+        tools.debug_print_arr_2dhex_1line(state)
+        print()
+
+        if curr_round != 10:
+            print(f'[ENCRYPT]: round{curr_round}: After MixColumns')
+            state = AESEncrypt.mix_cols(state)
+            tools.debug_print_arr_2dhex_1line(state)
+            print()
+
+        print(f'[ENCRYPT]: round{curr_round}: Round key Value')
+        round_key = AESEncrypt.extract_key(aes_keys[curr_round])
+        state = tools.xor_2d(state, round_key)
+        tools.debug_print_arr_2dhex_1line(round_key)
+        print()
+
+    print(f'AES Encrypt Complete')
+    tools.debug_print_arr_2dhex_1line(state)
+    print()
+
+    print(f'[START] AES Decrypt')
+
+    round_key = AESEncrypt.extract_key(aes_keys[10])
+
+    print(f'[DECRYPT] round{0}: iinput')
+    tools.debug_print_arr_2dhex_1line(state)
+    print()
+
+    print(f'[DECRYPT] round{0}: ik_sch')
+    tools.debug_print_arr_2dhex_1line(round_key)
+    print()
+
+    state = tools.xor_2d(state, round_key)
+
+    for inv_curr_round in range(9,-1,-1):
+
+        print(f'[DECRYPT] round{10 - inv_curr_round}: istart')
+        tools.debug_print_arr_2dhex_1line(state)
+        print()
+
+        print(f'[DECRYPT] round{10 - inv_curr_round}: is_row')
+        AESEncrypt.shift_rows_inv(state)
+        tools.debug_print_arr_2dhex_1line(state)
+        print()
+
+        print(f'[DECRYPT] round{10 - inv_curr_round}: is_box')
+        AESEncrypt.s_box_inv_sub(state)
+        tools.debug_print_arr_2dhex_1line(state)
+        print()
+
+        round_key = AESEncrypt.extract_key(aes_keys[inv_curr_round])
+
+        print(f'[DECRYPT] round{10 - inv_curr_round}: ik_sch')
+        tools.debug_print_arr_2dhex_1line(round_key)
+        print()
+
+        print(f'[DECRYPT] round{10 - inv_curr_round}: ik_add')
+        state = tools.xor_2d(state, round_key)
+        tools.debug_print_arr_2dhex_1line(state)
+        print()
+
+        if inv_curr_round != 0:
+            print(f'[DECRYPT] round{10 - inv_curr_round}: i_mix_cols')
+            state = AESEncrypt.inv_mix_cols(state)
+            tools.debug_print_arr_2dhex_1line(state)
+            print()
+
+    print(f'AES Decrypt Complete')
+    tools.debug_print_arr_2dhex_1line(state)
+    print()
+
 if __name__ == '__main__':
     print("-------------------------------")
     print("CSCI-531 AES-128 Implementation")
     print("-------------------------------")
-    arr_key = tools.read_file("../input/key.txt")
-    tools.debug_print_arr_ascii(arr_key)
+    #arr_key = tools.read_file("../input/key.txt")
+    #tools.debug_print_arr_ascii(arr_key)
 
-    arr_pt = tools.read_file("../input/plaintext.txt")
-    tools.debug_print_arr_ascii(arr_pt)
-
-"""
-    arr = [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xf, 0x10, 0x11]
-    arr2 = [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xf, 0x10, 0x11]
-    tools.debug_print_arr(arr)
-    result = tools.xor(arr, arr2)
-    tools.debug_print_arr(result)
-"""
-
-
+    #arr_pt = tools.read_file("../input/plaintext.txt")
+    #tools.debug_print_arr_ascii(arr_pt)
+    test_aes()
